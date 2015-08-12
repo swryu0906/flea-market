70e3a6efc42c95b384bb9a3e2e0c7ec31db04e7a
Sungwon Ryu <swryu0906@gmail.com>
2015-08-12 14:16:37 -0400

    Set up paperclip for listing image

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ActiveRecord::Base
+	has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "no-image-available.png"
+	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
 end

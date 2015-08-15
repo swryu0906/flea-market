26e4316358b402891c83a347ccbe01b3ecf8515c
Sungwon Ryu <swryu0906@gmail.com>
2015-08-14 23:05:26 -0400

    Add fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814222036) do
+ActiveRecord::Schema.define(version: 20150815024131) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20150814222036) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

68449b3786f4291fc9b9628a6a9ea5f4cf307873
Sungwon Ryu <swryu0906@gmail.com>
2015-08-20 16:09:51 -0400

    Enable users to place orders and stylize the order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,34 +1,44 @@
-class OrdersController < ApplicationController
+ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  #before_action :set_listing
+  before_action :authenticate_user!
 
   # GET /orders
   # GET /orders.json
   def index
     @orders = Order.all
+    @listing = Listing.find(params[:listing_id])
   end
 
   # GET /orders/1
   # GET /orders/1.json
   def show
+    @listing = Listing.find(params[:listing_id])
   end
 
   # GET /orders/new
   def new
     @order = Order.new
+    @listing = Listing.find(params[:listing_id])
   end
 
   # GET /orders/1/edit
   def edit
+    @listing = Listing.find(params[:listing_id])
   end
 
   # POST /orders
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @listing = Listing.find(params[:listing_id])
+    @order.listing_id = @listing.id
+    @order.seller_id = @listing.user_id
+    @order.buyer_id = current_user.id
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.html { redirect_to listing_orders_path(@listing), notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
@@ -40,9 +50,10 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
   def update
+    @listing = Listing.find(params[:listing_id])
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+        format.html { redirect_to listing_orders_path(@listing), notice: 'Order was successfully updated.' }
         format.json { render :show, status: :ok, location: @order }
       else
         format.html { render :edit }
@@ -54,9 +65,10 @@ class OrdersController < ApplicationController
   # DELETE /orders/1
   # DELETE /orders/1.json
   def destroy
+    @listing = Listing.find(params[:listing_id])
     @order.destroy
     respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.html { redirect_to listing_orders_path(@listing), notice: 'Order was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -66,6 +78,10 @@ class OrdersController < ApplicationController
     def set_order
       @order = Order.find(params[:id])
     end
+
+    #def set_listing
+      #@listing = Listing.find(params[:listing_id])
+    #end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
